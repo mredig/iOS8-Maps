af99d14889e18e6a1fd5d6c893212af15e2ff88b
Michael Redig <mredig@gmail.com>
2019-10-03 12:45:58 -0500

    refactored quake class to its own file

---
+++ iOS8-Quakes/iOS8-Quakes/Quake.swift
@@ -0,0 +1,52 @@
+//
+//  Quake.swift
+//  iOS8-Quakes
+//
+//  Created by Michael Redig on 10/3/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Foundation
+
+class QuakeResults: Decodable {
+	let features: [Quake]
+}
+
+class Quake: NSObject, Decodable {
+	let mag: Double
+	let place: String
+	let time: Date
+	let longitude: Double
+	let latitude: Double
+
+	enum QuakeKeys: String, CodingKey {
+		case properties
+		case geometry
+
+		enum PropertiesKeys: String, CodingKey {
+			case mag
+			case place
+			case time
+		}
+
+		enum GeographyKeys: String, CodingKey {
+			case coordinates
+		}
+	}
+
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: QuakeKeys.self)
+		let propertiesContainer = try container.nestedContainer(keyedBy: QuakeKeys.PropertiesKeys.self, forKey: .properties)
+		let geoContainer = try container.nestedContainer(keyedBy: QuakeKeys.GeographyKeys.self, forKey: .geometry)
+		var coordinateContainer = try geoContainer.nestedUnkeyedContainer(forKey: .coordinates)
+
+		self.mag = try propertiesContainer.decode(Double.self, forKey: .mag)
+		self.place = try propertiesContainer.decode(String.self, forKey: .place)
+		self.time = try propertiesContainer.decode(Date.self, forKey: .time)
+
+		self.longitude = try coordinateContainer.decode(Double.self)
+		self.latitude = try coordinateContainer.decode(Double.self)
+
+		super.init()
+	}
+}
--- iOS8-Quakes/iOS8-QuakesTests/iOS8_QuakesTests.swift
+++ iOS8-Quakes/iOS8-QuakesTests/iOS8_QuakesTests.swift
@@ -7,49 +7,7 @@
 //
 
 import XCTest
-
-class QuakeResults: Decodable {
-	let features: [Quake]
-}
-
-class Quake: NSObject, Decodable {
-	let mag: Double
-	let place: String
-	let time: Date
-	let longitude: Double
-	let latitude: Double
-
-	enum QuakeKeys: String, CodingKey {
-		case properties
-		case geometry
-
-		enum PropertiesKeys: String, CodingKey {
-			case mag
-			case place
-			case time
-		}
-
-		enum GeographyKeys: String, CodingKey {
-			case coordinates
-		}
-	}
-
-	required init(from decoder: Decoder) throws {
-		let container = try decoder.container(keyedBy: QuakeKeys.self)
-		let propertiesContainer = try container.nestedContainer(keyedBy: QuakeKeys.PropertiesKeys.self, forKey: .properties)
-		let geoContainer = try container.nestedContainer(keyedBy: QuakeKeys.GeographyKeys.self, forKey: .geometry)
-		var coordinateContainer = try geoContainer.nestedUnkeyedContainer(forKey: .coordinates)
-
-		self.mag = try propertiesContainer.decode(Double.self, forKey: .mag)
-		self.place = try propertiesContainer.decode(String.self, forKey: .place)
-		self.time = try propertiesContainer.decode(Date.self, forKey: .time)
-
-		self.longitude = try coordinateContainer.decode(Double.self)
-		self.latitude = try coordinateContainer.decode(Double.self)
-
-		super.init()
-	}
-}
+@testable import iOS8_Quakes
 
 class iOS8_QuakesTests: XCTestCase {
 
